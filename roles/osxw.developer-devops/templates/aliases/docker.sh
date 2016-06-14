86754678b743e1f8b56235da68fe2d9ec04df48f
Luis Hdez <luis.munoz.hdez@gmail.com>
2016-06-14 22:03:45 +0200

    Update alias for docker

--- roles/osxw.developer-devops/templates/aliases/docker.sh
+++ roles/osxw.developer-devops/templates/aliases/docker.sh
@@ -1,4 +1,12 @@
-# Docker
-alias docker.stop.all="docker stop $(docker ps -a -q)"
-alias docker.rm.all="docker rm $(docker ps -a -q)"
-alias docker.volume.rm.all="docker volume rm $(docker volume ls -q)"
+# Docker: dkr
+function dkr.stop.all {
+    docker stop `docker ps -a -q`
+}
+
+function dkr.rm.all {
+    docker rm `docker ps -a -q`
+}
+
+function dkr.volume.rm.all {
+    docker volume rm `docker volume ls -q`
+}
